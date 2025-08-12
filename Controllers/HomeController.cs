1a7400ac1f3a06555aa5438d4dc9e05e60ba1f63
BRISA JULIETA MONASTIRIDIS VOROTNICOFF <49192786@ort>
2025-08-12 12:01:46 -0300

    home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,9 +25,58 @@ public class HomeController : Controller
    
     return View("Tarea");
     }
+  public IActionResult VerTareas()
+{
+    int id = int.Parse(HttpContext.Session.GetString("idUser"));
+    ViewBag.Tareas = BD.VerTareas(id);
+   
+    return View("ListaTareas");
+}
+public IActionResult ModificarTarea(int idTarea)
+{
+    int id = int.Parse(HttpContext.Session.GetString("idUser"));
+    ViewBag.Tareas = BD.ModificarTarea(idTarea);
+   
+    return View("Tarea");
+}
+public IActionResult FinalizarTarea(int idTarea)
+{
+    int id = int.Parse(HttpContext.Session.GetString("idUser"));
+    ViewBag.Tareas = BD.FinalizarTarea(idTarea);
+   
+    return View("Tarea");
+}
+
+public IActionResult EliminarTarea(int idTarea)
+{
+    int id = int.Parse(HttpContext.Session.GetString("idUser"));
+    ViewBag.Tareas = BD.EliminarTarea(idTarea);
+   
+    return View("Tarea");
+}
+
+
+
+
+
+
+public IActionResult AgregarTarea(int Id, string Titulo, string Descripcion,DateTime Fecha, bool Finalización, int IdUsuario)
+{
+Tarea NuevaTarea= new Tarea
+{
+    Id = Id,
+    Titulo= Titulo,
+    Descripcion= Descripcion,
+    Fecha= Fecha,
+    Finalización= Finalización,
+    IdUsuario= IdUsuario ,
+ 
+};
+}
     public IActionResult Logout(){
       HttpContext.Session.Clear();
       return RedirectToAction("Index");
       
     }
 }
+
